17f4dbf863e7d340586b16630de64be318930885
philiptang42 <philiptang42@gmail.com>
2016-01-07 21:38:20 -0500

    part 2 admit! method

--- movie_sell_out.rb
+++ movie_sell_out.rb
@@ -1,9 +1,23 @@
+require 'pry'
+
 class Theater
   attr_accessor :patrons, :seats
 
-  def initialize(seats)
+  def initialize(seats, patrons = 0)
     @seats = seats
     @patrons = patrons
   end
 
+  def admit!(admittance = nil)
+    if patrons >= seats
+      puts "Error! Not enough seats for the requested amount of people."
+    elsif admittance.nil?
+      @patrons = patrons + 1
+    else
+      @patrons = patrons + admittance
+    end
+  end
+
 end
+
+binding.pry
